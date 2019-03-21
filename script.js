32c50202c5b10f6e003d2f2ddc822d6219eea2f9
Shawn Holcomb <sholcmb@gmail.com>
2019-03-21 01:35:37 -0500

    Front end work completed.

---
+++ script.js
@@ -0,0 +1,402 @@
+$(document).ready(function () {
+
+    var clickId = this.id;
+
+function clicker() {
+    $("button").on("click", function () {
+        console.log(this.id);
+        // $(b).text(this.value).val(this.value);
+    });
+}
+
+clicker();
+
+
+
+    $("#g1-1a").on("click", function () {
+        $("#g2-1a").text(this.value).val(this.value);
+    });
+    $("#g1-1b").on("click", function () {
+        $("#g2-1a").text(this.value).val(this.value);
+    });
+    $("#g1-2a").on("click", function () {
+        $("#g2-1b").text(this.value).val(this.value);
+    });
+    $("#g1-2b").on("click", function () {
+        $("#g2-1b").text(this.value).val(this.value);
+    });
+    $("#g1-3a").on("click", function () {
+        $("#g2-2a").text(this.value).val(this.value);
+    });
+    $("#g1-3b").on("click", function () {
+        $("#g2-2a").text(this.value).val(this.value);
+    });
+    $("#g1-4a").on("click", function () {
+        $("#g2-2b").text(this.value).val(this.value);
+    });
+    $("#g1-4b").on("click", function () {
+        $("#g2-2b").text(this.value).val(this.value);
+    });
+    $("#g1-5a").on("click", function () {
+        $("#g2-3a").text(this.value).val(this.value);
+    });
+    $("#g1-5b").on("click", function () {
+        $("#g2-3a").text(this.value).val(this.value);
+    });
+    $("#g1-6a").on("click", function () {
+        $("#g2-3b").text(this.value).val(this.value);
+    });
+    $("#g1-6b").on("click", function () {
+        $("#g2-3b").text(this.value).val(this.value);
+    });
+    $("#g1-7a").on("click", function () {
+        $("#g2-4a").text(this.value).val(this.value);
+    });
+    $("#g1-7b").on("click", function () {
+        $("#g2-4a").text(this.value).val(this.value);
+    });
+    $("#g1-8a").on("click", function () {
+        $("#g2-4b").text(this.value).val(this.value);
+    });
+    $("#g1-8b").on("click", function () {
+        $("#g2-4b").text(this.value).val(this.value);
+    });
+    $("#g1-9a").on("click", function () {
+        $("#g2-5a").text(this.value).val(this.value);
+    });
+    $("#g1-9b").on("click", function () {
+        $("#g2-5a").text(this.value).val(this.value);
+    });
+    $("#g1-10a").on("click", function () {
+        $("#g2-5b").text(this.value).val(this.value);
+    });
+    $("#g1-10b").on("click", function () {
+        $("#g2-5b").text(this.value).val(this.value);
+    });
+    $("#g1-11a").on("click", function () {
+        $("#g2-6a").text(this.value).val(this.value);
+    });
+    $("#g1-11b").on("click", function () {
+        $("#g2-6a").text(this.value).val(this.value);
+    });
+    $("#g1-12a").on("click", function () {
+        $("#g2-6b").text(this.value).val(this.value);
+    });
+    $("#g1-12b").on("click", function () {
+        $("#g2-6b").text(this.value).val(this.value);
+    });
+    $("#g1-13a").on("click", function () {
+        $("#g2-7a").text(this.value).val(this.value);
+    });
+    $("#g1-13b").on("click", function () {
+        $("#g2-7a").text(this.value).val(this.value);
+    });
+    $("#g1-14a").on("click", function () {
+        $("#g2-7b").text(this.value).val(this.value);
+    });
+    $("#g1-14b").on("click", function () {
+        $("#g2-7b").text(this.value).val(this.value);
+    });
+    $("#g1-15a").on("click", function () {
+        $("#g2-8a").text(this.value).val(this.value);
+    });
+    $("#g1-15b").on("click", function () {
+        $("#g2-8a").text(this.value).val(this.value);
+    });
+    $("#g1-16a").on("click", function () {
+        $("#g2-8b").text(this.value).val(this.value);
+    });
+    $("#g1-16b").on("click", function () {
+        $("#g2-8b").text(this.value).val(this.value);
+    });
+    $("#g1-17a").on("click", function () {
+        $("#g2-9a").text(this.value).val(this.value);
+    });
+    $("#g1-17b").on("click", function () {
+        $("#g2-9a").text(this.value).val(this.value);
+    });
+    $("#g1-18a").on("click", function () {
+        $("#g2-9b").text(this.value).val(this.value);
+    });
+    $("#g1-18b").on("click", function () {
+        $("#g2-9b").text(this.value).val(this.value);
+    });
+    $("#g1-19a").on("click", function () {
+        $("#g2-10a").text(this.value).val(this.value);
+    });
+    $("#g1-19b").on("click", function () {
+        $("#g2-10a").text(this.value).val(this.value);
+    });
+    $("#g1-20a").on("click", function () {
+        $("#g2-10b").text(this.value).val(this.value);
+    });
+    $("#g1-20b").on("click", function () {
+        $("#g2-10b").text(this.value).val(this.value);
+    });
+    $("#g1-21a").on("click", function () {
+        $("#g2-11a").text(this.value).val(this.value);
+    });
+    $("#g1-21b").on("click", function () {
+        $("#g2-11a").text(this.value).val(this.value);
+    });
+    $("#g1-22a").on("click", function () {
+        $("#g2-11b").text(this.value).val(this.value);
+    });
+    $("#g1-22b").on("click", function () {
+        $("#g2-11b").text(this.value).val(this.value);
+    });
+    $("#g1-23a").on("click", function () {
+        $("#g2-12a").text(this.value).val(this.value);
+    });
+    $("#g1-23b").on("click", function () {
+        $("#g2-12a").text(this.value).val(this.value);
+    });
+    $("#g1-24a").on("click", function () {
+        $("#g2-12b").text(this.value).val(this.value);
+    });
+    $("#g1-24b").on("click", function () {
+        $("#g2-12b").text(this.value).val(this.value);
+    });
+    $("#g1-25a").on("click", function () {
+        $("#g2-13a").text(this.value).val(this.value);
+    });
+    $("#g1-25b").on("click", function () {
+        $("#g2-13a").text(this.value).val(this.value);
+    });
+    $("#g1-26a").on("click", function () {
+        $("#g2-13b").text(this.value).val(this.value);
+    });
+    $("#g1-26b").on("click", function () {
+        $("#g2-13b").text(this.value).val(this.value);
+    });
+    $("#g1-27a").on("click", function () {
+        $("#g2-14a").text(this.value).val(this.value);
+    });
+    $("#g1-27b").on("click", function () {
+        $("#g2-14a").text(this.value).val(this.value);
+    });
+    $("#g1-28a").on("click", function () {
+        $("#g2-14b").text(this.value).val(this.value);
+    });
+    $("#g1-28b").on("click", function () {
+        $("#g2-14b").text(this.value).val(this.value);
+    });
+    $("#g1-29a").on("click", function () {
+        $("#g2-15a").text(this.value).val(this.value);
+    });
+    $("#g1-29b").on("click", function () {
+        $("#g2-15a").text(this.value).val(this.value);
+    });
+    $("#g1-30a").on("click", function () {
+        $("#g2-15b").text(this.value).val(this.value);
+    });
+    $("#g1-30b").on("click", function () {
+        $("#g2-15b").text(this.value).val(this.value);
+    });
+    $("#g1-31a").on("click", function () {
+        $("#g2-16a").text(this.value).val(this.value);
+    });
+    $("#g1-31b").on("click", function () {
+        $("#g2-16a").text(this.value).val(this.value);
+    });
+    $("#g1-32a").on("click", function () {
+        $("#g2-16b").text(this.value).val(this.value);
+    });
+    $("#g1-32b").on("click", function () {
+        $("#g2-16b").text(this.value).val(this.value);
+    });
+
+    $("#g2-1a").on("click", function () {
+        $("#g3-1a").text(this.value).val(this.value);
+    });
+    $("#g2-1b").on("click", function () {
+        $("#g3-1a").text(this.value).val(this.value);
+    });
+    $("#g2-2a").on("click", function () {
+        $("#g3-1b").text(this.value).val(this.value);
+    });
+    $("#g2-2b").on("click", function () {
+        $("#g3-1b").text(this.value).val(this.value);
+    });
+    $("#g2-3a").on("click", function () {
+        $("#g3-2a").text(this.value).val(this.value);
+    });
+    $("#g2-3b").on("click", function () {
+        $("#g3-2a").text(this.value).val(this.value);
+    });
+    $("#g2-4a").on("click", function () {
+        $("#g3-2b").text(this.value).val(this.value);
+    });
+    $("#g2-4b").on("click", function () {
+        $("#g3-2b").text(this.value).val(this.value);
+    });
+    $("#g2-5a").on("click", function () {
+        $("#g3-3a").text(this.value).val(this.value);
+    });
+    $("#g2-5b").on("click", function () {
+        $("#g3-3a").text(this.value).val(this.value);
+    });
+    $("#g2-6a").on("click", function () {
+        $("#g3-3b").text(this.value).val(this.value);
+    });
+    $("#g2-6b").on("click", function () {
+        $("#g3-3b").text(this.value).val(this.value);
+    });
+    $("#g2-7a").on("click", function () {
+        $("#g3-4a").text(this.value).val(this.value);
+    });
+    $("#g2-7b").on("click", function () {
+        $("#g3-4a").text(this.value).val(this.value);
+    });
+    $("#g2-8a").on("click", function () {
+        $("#g3-4b").text(this.value).val(this.value);
+    });
+    $("#g2-8b").on("click", function () {
+        $("#g3-4b").text(this.value).val(this.value);
+    });
+    $("#g2-9a").on("click", function () {
+        $("#g3-5a").text(this.value).val(this.value);
+    });
+    $("#g2-9b").on("click", function () {
+        $("#g3-5a").text(this.value).val(this.value);
+    });
+    $("#g2-10a").on("click", function () {
+        $("#g3-5b").text(this.value).val(this.value);
+    });
+    $("#g2-10b").on("click", function () {
+        $("#g3-5b").text(this.value).val(this.value);
+    });
+    $("#g2-11a").on("click", function () {
+        $("#g3-6a").text(this.value).val(this.value);
+    });
+    $("#g2-11b").on("click", function () {
+        $("#g3-6a").text(this.value).val(this.value);
+    });
+    $("#g2-12a").on("click", function () {
+        $("#g3-6b").text(this.value).val(this.value);
+    });
+    $("#g2-12b").on("click", function () {
+        $("#g3-6b").text(this.value).val(this.value);
+    });
+    $("#g2-13a").on("click", function () {
+        $("#g3-7a").text(this.value).val(this.value);
+    });
+    $("#g2-13b").on("click", function () {
+        $("#g3-7a").text(this.value).val(this.value);
+    });
+    $("#g2-14a").on("click", function () {
+        $("#g3-7b").text(this.value).val(this.value);
+    });
+    $("#g2-14b").on("click", function () {
+        $("#g3-7b").text(this.value).val(this.value);
+    });
+    $("#g2-15a").on("click", function () {
+        $("#g3-8a").text(this.value).val(this.value);
+    });
+    $("#g2-15b").on("click", function () {
+        $("#g3-8a").text(this.value).val(this.value);
+    });
+    $("#g2-16a").on("click", function () {
+        $("#g3-8b").text(this.value).val(this.value);
+    });
+    $("#g2-16b").on("click", function () {
+        $("#g3-8b").text(this.value).val(this.value);
+    });
+    $("#g3-1a").on("click", function () {
+        $("#g4-1a").text(this.value).val(this.value);
+    });
+    $("#g3-1b").on("click", function () {
+        $("#g4-1a").text(this.value).val(this.value);
+    });
+    $("#g3-2a").on("click", function () {
+        $("#g4-1b").text(this.value).val(this.value);
+    });
+    $("#g3-2b").on("click", function () {
+        $("#g4-1b").text(this.value).val(this.value);
+    });
+    $("#g3-3a").on("click", function () {
+        $("#g4-2a").text(this.value).val(this.value);
+    });
+    $("#g3-3b").on("click", function () {
+        $("#g4-2a").text(this.value).val(this.value);
+    });
+    $("#g3-4a").on("click", function () {
+        $("#g4-2b").text(this.value).val(this.value);
+    });
+    $("#g3-4b").on("click", function () {
+        $("#g4-2b").text(this.value).val(this.value);
+    });
+    $("#g3-5a").on("click", function () {
+        $("#g4-3a").text(this.value).val(this.value);
+    });
+    $("#g3-5b").on("click", function () {
+        $("#g4-3a").text(this.value).val(this.value);
+    });
+    $("#g3-6a").on("click", function () {
+        $("#g4-3b").text(this.value).val(this.value);
+    });
+    $("#g3-6b").on("click", function () {
+        $("#g4-3b").text(this.value).val(this.value);
+    });
+    $("#g3-7a").on("click", function () {
+        $("#g4-4a").text(this.value).val(this.value);
+    });
+    $("#g3-7b").on("click", function () {
+        $("#g4-4a").text(this.value).val(this.value);
+    });
+    $("#g3-8a").on("click", function () {
+        $("#g4-4b").text(this.value).val(this.value);
+    });
+    $("#g3-8b").on("click", function () {
+        $("#g4-4b").text(this.value).val(this.value);
+    });
+    $("#g4-1a").on("click", function () {
+        $("#g5-1a").text(this.value).val(this.value);
+    });
+    $("#g4-1b").on("click", function () {
+        $("#g5-1a").text(this.value).val(this.value);
+    });
+    $("#g4-2a").on("click", function () {
+        $("#g5-1b").text(this.value).val(this.value);
+    });
+    $("#g4-2b").on("click", function () {
+        $("#g5-1b").text(this.value).val(this.value);
+    });
+    $("#g4-3a").on("click", function () {
+        $("#g5-2a").text(this.value).val(this.value);
+    });
+    $("#g4-3b").on("click", function () {
+        $("#g5-2a").text(this.value).val(this.value);
+    });
+    $("#g4-4a").on("click", function () {
+        $("#g5-2b").text(this.value).val(this.value);
+    });
+    $("#g4-4b").on("click", function () {
+        $("#g5-2b").text(this.value).val(this.value);
+    });
+    $("#g5-1a").on("click", function () {
+        $("#g6-1a").text(this.value).val(this.value);
+    });
+    $("#g5-1b").on("click", function () {
+        $("#g6-1a").text(this.value).val(this.value);
+    });
+    $("#g5-2a").on("click", function () {
+        $("#g6-1b").text(this.value).val(this.value);
+    });
+    $("#g5-2b").on("click", function () {
+        $("#g6-1b").text(this.value).val(this.value);
+    });
+    $("#g6-1a").on("click", function () {
+        $("#g7").text(this.value).val(this.value);
+    });
+    $("#g6-1b").on("click", function () {
+        $("#g7").text(this.value).val(this.value);
+    });
+
+    $("#clear").on("click", function () {
+        $(".team-top").empty();
+        $(".team-bottom").empty();
+        $("#team-name").empty();
+    });
+
+});
